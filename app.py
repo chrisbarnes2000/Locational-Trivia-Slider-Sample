dcaff024d77a159c6514e0862be548c984939a4a
Christopher Barnes <cbarnes7404@yahoo.com>
2019-11-05 22:39:59 -0800

    add about page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,
+from flask import Flask, render_template, request
 import os
 
 app = Flask(__name__)
@@ -6,9 +6,15 @@ app = Flask(__name__)
 # INDEX
 @app.route('/')
 @app.route('/index')
-def items_index():
+def index():
     """Show the Home page."""
-    return render_template('index.html')  # , items=items.find()
+    return render_template('index.html')
+
+# ABOUT
+@app.route('/about')
+def about():
+    """Show the About page."""
+    return render_template('about.html')
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=os.environ.get('PORT', 5000), debug=True)
